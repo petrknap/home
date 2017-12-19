98f827dfb126a155eaa5c22e3accffc0eb214133
Petr Knap <dev@petrknap.cz>
2017-12-19 23:05:05 +0100

    Added backup script

---
+++ roles/bash/files/backup.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e -x
+
+CURRENT_USER=$USER
+if [ -e ~/backup.tmp ]
+then
+	sudo rm ~/backup.tmp
+fi
+
+ls -aR ~/Downloads > ~/Downloads.ls || true
+ls -aR ~/Videos > ~/Videos.ls || true
+ls -aR ~/VirtualBox\ VMs > ~/VirtualBox\ VMs.ls || true
+sudo tar -cvzf ~/backup.tmp \
+	$(realpath $0) \
+	/etc \
+	~/.apps \
+	~/.bin \
+	~/.config \
+	~/.local/share/applications \
+	~/.thunderbird \
+	~/.PhpStorm* \
+	~/.PyCharm* \
+	~/*.ls \
+	$(find ~/* -maxdepth 0 -type d -not -path "*/Downloads" -not -path "*/Videos" -not -path "*/VirtualBox VMs") \
+&& rm ~/*.ls;
+sudo chown $CURRENT_USER ~/backup.tmp
+
+if [ -e ~/backup.tar.gz ]
+then
+	rm ~/backup.tar.gz
+fi
+mv ~/backup.tmp ~/backup.tar.gz
