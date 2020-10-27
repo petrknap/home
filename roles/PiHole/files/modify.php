d79daec668941d1d1a6ca57bc01562b57f7b8dcc
Petr Knap <dev@petrknap.cz>
2020-10-27 19:33:18 +0100

    Added modify.php script

---
+++ roles/PiHole/files/modify.php
@@ -0,0 +1,14 @@
+<?php
+
+$list = @$_GET['list'];
+$ip = @$_GET['ip'];
+
+if ($list) {
+    $hosts = file_get_contents($list);
+    $hosts = preg_replace('/^([^#\s]+\s+)?(.*)$/m', $ip ? "{$ip} \$2" : '$2', $hosts);
+} else {
+    $hosts = '# Usage: modify.php?from=https://example.org/hosts[&ip=192.168.0.1]';
+}
+
+header('Content-Type: text/plain');
+echo $hosts;
