8dcab106423d749b22687d669c5d56ad9230b2ad
Petr Knap <8299754+petrknap@users.noreply.github.com>
2022-05-27 16:19:52 +0200

    refactor: changed brackets and added header

--- roles/Git/files/macros.bash
+++ roles/Git/files/macros.bash
@@ -1,14 +1,15 @@
+#!/usr/bin/env bash
 git() {
-    if [ "$1" == "help" ] && [ "$2" == "" ]
+    if [[ "$1" == "help" ]] && [[ "$2" == "" ]]
     then
         command git "$@"
         command echo ""
         command echo "Macros:"
         command echo "   git branch --clear <branch>"
         command echo "      Locally deletes branches which are merged to <branch>."
-    elif [ "$1" == "branch" ] && [ "$2" == "--clear" ]
+    elif [[ "$1" == "branch" ]] && [[ "$2" == "--clear" ]]
     then
-        if [ "$3" == "" ]
+        if [[ "$3" == "" ]]
         then
             command echo "ERROR!"
             command echo "    You must specify branch as 3rd parameter."
@@ -17,7 +18,7 @@ git() {
             for MERGED_BRANCH in `command git branch --merged "$MERGED_TO" | grep -v "$MERGED_TO" | grep -v "master" | grep -v "*"`
             do
                 read -p "Are you sure you would like to delete $MERGED_BRANCH merged to $MERGED_TO? [Y/n] " CHOICE
-                if [ "$CHOICE" == "Y" ] || [ "$CHOICE" == "y" ] || [ "$CHOICE" == "" ]
+                if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]] || [[ "$CHOICE" == "" ]]
                 then
                     command git branch --delete "$MERGED_BRANCH"
                 fi
