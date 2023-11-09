6bb3346fc6fcf1814c0a702e39140f3be8bbec60
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-11-09 21:54:57 +0100

    refactor: improved backup script

--- roles/workstation/files/backup.bash
+++ roles/workstation/files/backup.bash
@@ -9,21 +9,25 @@ fi
 ls -aR ~/.apps > ~/.apps.ls || true
 ls -aR ~/Downloads > ~/Downloads.ls || true
 ls -aR ~/Videos > ~/Videos.ls || true
+ls -aR ~/github.com > ~/github.com.ls || true
+ls -aR ~/snap > ~/snap.ls || true
 find ~ -maxdepth 1 \
     -not -path ~ \
-    -not -path ~/backup.key -not -path ~/backup.tmp -not -path ~/backup.tar -not -path ~/backup.tar.gz -not -path ~/backup.tar.gz.gpg \
+    -not -path ~/backup.key -not -path ~/backup.tmp -not -path ~/backup.tar -not -path ~/backup.tar.xz -not -path ~/backup.tar.xz.gpg \
     -not -path ~/.apps \
     -not -path ~/.cache \
     -not -path ~/Downloads \
     -not -path ~/Videos \
+    -not -path ~/github.com \
+    -not -path ~/snap \
     | sed 's/.*/"&"/' \
-    | xargs sudo tar -cz \
+    | xargs -t sudo tar --checkpoint=250 --create --xz \
         /etc \
-    | gpg --symmetric --passphrase-file ~/backup.key --batch --output ~/backup.tmp \
+    | gpg --verbose --symmetric --passphrase-file ~/backup.key --batch --output ~/backup.tmp \
 && rm ~/*.ls
 
-if [ -e ~/backup.tar.gz.gpg ]
+if [ -e ~/backup.tar.xz.gpg ]
 then
-    rm ~/backup.tar.gz.gpg
+    rm ~/backup.tar.xz.gpg
 fi
-mv ~/backup.tmp ~/backup.tar.gz.gpg
+mv ~/backup.tmp ~/backup.tar.xz.gpg
