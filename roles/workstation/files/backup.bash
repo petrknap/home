220f5b38f8c38abb28d82a167f1aaedd1bd51bfb
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-11-09 18:14:24 +0100

    refactor: improved backup script

--- roles/workstation/files/backup.bash
+++ roles/workstation/files/backup.bash
@@ -6,11 +6,14 @@ then
     rm ~/backup.tmp
 fi
 
+ls -aR ~/.apps > ~/.apps.ls || true
 ls -aR ~/Downloads > ~/Downloads.ls || true
 ls -aR ~/Videos > ~/Videos.ls || true
 find ~ -maxdepth 1 \
     -not -path ~ \
     -not -path ~/backup.key -not -path ~/backup.tmp -not -path ~/backup.tar -not -path ~/backup.tar.gz -not -path ~/backup.tar.gz.gpg \
+    -not -path ~/.apps \
+    -not -path ~/.cache \
     -not -path ~/Downloads \
     -not -path ~/Videos \
     | sed 's/.*/"&"/' \
