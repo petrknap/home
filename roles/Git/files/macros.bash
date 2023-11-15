426fe63409cee31bd6d33d0287a3949348a6ef3b
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-11-15 16:54:06 +0100

    chore: masters renamed to mains

--- roles/Git/files/macros.bash
+++ roles/Git/files/macros.bash
@@ -15,7 +15,7 @@ git() {
             command echo "    You must specify branch as 3rd parameter."
         else
             MERGED_TO="$3"
-            for MERGED_BRANCH in `command git branch --merged "$MERGED_TO" | grep -v "$MERGED_TO" | grep -v "master" | grep -v "*"`
+            for MERGED_BRANCH in `command git branch --merged "$MERGED_TO" | grep -v "$MERGED_TO" | grep -v "main" | grep -v "*"`
             do
                 read -p "Are you sure you would like to delete $MERGED_BRANCH merged to $MERGED_TO? [Y/n] " CHOICE
                 if [[ "$CHOICE" == "Y" ]] || [[ "$CHOICE" == "y" ]] || [[ "$CHOICE" == "" ]]
