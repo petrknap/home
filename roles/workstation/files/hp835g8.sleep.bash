3a42ec8fc028f2be19a88f3e7e3a4916439ad7d4
Petr Knap <8299754+petrknap@users.noreply.github.com>
2026-06-19 18:06:55 +0200

    chore: changes for Ubuntu 26 and HP 835 G8

--- roles/workstation/files/hp835g8.sleep.bash
+++ roles/workstation/files/hp835g8.sleep.bash
@@ -2,9 +2,11 @@
 
 case "${1}" in
   pre)
-     powerprofilesctl set balanced  # fixes AMD Ryzen boost
+     powerprofilesctl set balanced  # fixes AMD Ryzen boost bug on wakeup
+     rfkill block all  # prevents exploits and battery drain
      ;;
   post)
     powerprofilesctl set power-saver
+    rfkill unblock wifi
     ;;
 esac
